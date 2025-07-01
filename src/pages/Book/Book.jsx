f87a0ecc4c10f1df0e5137b4e64b3175b7e54b7b
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-07-01 14:23:27 +0600

    Shakil

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,4 +1,5 @@
 import React, { use } from 'react';
+import { FaStarHalfAlt } from "react-icons/fa";
 
 // const Book = ({bookPromise}) => {
 //     const data=use (bookPromise);
@@ -13,24 +14,25 @@ import React, { use } from 'react';
 
 const Book = ({singleBook}) => {
 
-    const {bookName,author,image,review,category,publisher}=singleBook
+    const {bookName,author,image,review,category,yearOfPublishing,rating}=singleBook
     
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="card  w-96 shadow-sm border bg-gray-600">
   <figure className='p-3'>
-    <img
+    <img className='h-[166px]'
       src={image}
       alt="Shoes" />
   </figure>
   <div className="card-body">
     <h2 className="card-title">
-      Card Title
-      <div className="badge badge-secondary">NEW</div>
+     {bookName}
+      <div className="badge badge-secondary ">{yearOfPublishing}</div>
     </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <p className='mx-auto'>{review}</p>
     <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+      <div className="badge badge-outline">{author}</div>
+      <div className="badge badge-outline">{category}</div>
+      <div className="badge badge-outline">{rating}<FaStarHalfAlt /></div>
     </div>
   </div>
         </div>
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -15,7 +15,7 @@ const Books = ({data}) => {
             <h1 className='text-3xl text-center p-6 font-bold'>Books</h1>
             <Suspense fallback={<span>Loading....</span>}>
             {/* <Book bookPromise={bookPromise}></Book> */}
-             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                 {
                 data.map((singleBook)=> <Book key={singleBook.bookId} singleBook={singleBook} ></Book>)
                 }
