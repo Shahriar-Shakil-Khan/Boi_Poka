fac48fff78d007ad89901e72d2aba508ac2917c6
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-07-01 14:10:02 +0600

    Shakil

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,17 +3,21 @@ import bookImage from '../../assets/pngwing 1.png'
 
 const Banner = () => {
     return (
-        <div className='flex items-center justify-around'>
-            <div>
-                <h1 className='font-bold text-3xl'>Books to freshen up <br /> your bookshelf</h1>
-                <button className='btn btn-primary mt-4'>View the list</button>
-            </div>
-
-            <div>
-                  <img src={bookImage} alt="" />
-            </div>
-        </div>
+        <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img
+      src={bookImage}
+      className="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div>
+      <h1 className="text-5xl font-bold">Books to freshen up <br /> your bookshelf</h1>
+      <button className="btn btn-primary mt-4">View the list</button>
+    </div>
+  </div>
+</div>
     );
 };
 
 export default Banner;
+
+
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,15 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
+import Book from '../Book/Book';
 
-const Books = () => {
-    const [allBooks,setAllBooks]=useState([]);
-    useEffect(()=>{
-     fetch("booksData.json")
-     .then(res=>res.json())
-     .then(data=>{setAllBooks(data)})
-    },[])
+const Books = ({data}) => {
+    // const [allBooks,setAllBooks]=useState([]);    
+    // useEffect(()=>{
+    //  fetch("booksData.json")
+    //  .then(res=>res.json())
+    //  .then(data=>{setAllBooks(data)})
+    // },[])
+
+    // const bookPromise=fetch('booksData.json').then(res=>res.json())
     return (
         <div>
-            <h1>bookssssss</h1>
+            <h1 className='text-3xl text-center p-6 font-bold'>Books</h1>
+            <Suspense fallback={<span>Loading....</span>}>
+            {/* <Book bookPromise={bookPromise}></Book> */}
+             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                data.map((singleBook)=> <Book key={singleBook.bookId} singleBook={singleBook} ></Book>)
+                }
+             </div>
+            </Suspense>
         </div>
     );
 };
